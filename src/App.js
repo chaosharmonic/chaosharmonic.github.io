62cd7805339e707f24ff26da0bf065c86b2b26f8
Sean <smorrissey@botkeeper.com>
2020-06-05 14:26:08 -0400

    Update bio

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import 'rbx/index.css'
 import 'bulmaswatch/cyborg/bulmaswatch.min.css'
 import './App.css'
 import { Container } from 'rbx'
+import Splash from './components/Splash.js'
 import About from './components/About.js'
-import Navigation from './components/Navigation.js'
 import Projects from './components/Projects'
 import BuiltWith from './components/BuiltWith'
-import Splash from './components/Splash.js'
+import Navigation from './components/Navigation.js'
 
 const App = () => {
   const [currentView, setCurrentView] = useState('home')
--- src/components/About.js
+++ src/components/About.js
@@ -2,8 +2,8 @@ import React from 'react'
 import { Section, Content, Title } from 'rbx'
 
 const bio = [
-  'I\'m a trained musician, turned one-man tech support desk, turned self-taught JavaScript developer. I mostly work with MERN-stack libraries (with some occasional dabblings in Vue), but have built a career on picking up new skillsets to solve the problems in front of me - whether that\'s streamlining POS deployments across a restaurant chain or picking apart Express routes in order to fix a UI.',
-  'In my free time, I\'m typically working out, gaming, shouting from the Internet rooftops about the need for a modern spec, or toying with an experiment or five. Some previous endeavors have included building and automating RGB lighting, code as music, cardio via DDR, and DIY cocktails.'
+  'I\'m a trained musician, turned one-man IT support desk, turned self-taught JavaScript developer. Since parlaying my tinkering habit into a profession, I\'ve built up a career on picking up new skillsets to solve the problems in front of me - ranging from streamlining POS deployments across a restaurant chain, to moving my entire workplace off BYOD, to picking apart Express routes in order to fix data flowing into a React app.',
+  'In my free time, I\'m typically working out, gaming, shouting from the Internet rooftops that chat needs a spec, or toying with an experiment or five. Some previous endeavors have included wiring and automating my own RGB lighting, fixing up a DDR pad just so that I could avoid going running, and perrennial attempts to finally nail down a free-standing handstand.'
 ].map((paragraph, key) => <p key={key}>{paragraph}</p>)
 
 const About = () => (
